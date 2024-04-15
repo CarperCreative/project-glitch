9e5102cc604c7e4d121b96b583b503dadcc6e9b3
opl <4833621+opl@users.noreply.github.com>
2024-04-15 21:13:27 +0200

    Initial commit

---
+++ src/client/kotlin/com/carpercreative/preventthespread/PreventTheSpreadClient.kt
@@ -0,0 +1,9 @@
+package com.carpercreative.preventthespread
+
+import net.fabricmc.api.ClientModInitializer
+
+object PreventTheSpreadClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpreadDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.carpercreative.preventthespread
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object PreventTheSpreadDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
