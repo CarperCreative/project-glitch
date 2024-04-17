77763e66067ac97ee34cbf7d2646b2d87687dc28
opl <4833621+opl@users.noreply.github.com>
2024-04-17 03:50:55 +0200

    Add creative menu item group

--- src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
@@ -3,6 +3,7 @@ package com.carpercreative.preventthespread
 import com.carpercreative.preventthespread.block.CancerBlock
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.MapColor
 import net.minecraft.block.piston.PistonBehavior
@@ -10,6 +11,7 @@ import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
@@ -21,6 +23,14 @@ object PreventTheSpread : ModInitializer {
 	val CANCER_BLOCK = CancerBlock(FabricBlockSettings.create().strength(4.0f).ticksRandomly().mapColor(MapColor.DARK_CRIMSON).pistonBehavior(PistonBehavior.BLOCK))
 	val CANCER_BLOCK_ITEM = BlockItem(CANCER_BLOCK, FabricItemSettings())
 
+	private val ITEM_GROUP = FabricItemGroup.builder()
+		.icon { ItemStack(CANCER_BLOCK_ITEM) }
+		.displayName(Text.translatable("itemGroup.$MOD_ID.default"))
+		.entries { context, entries ->
+			entries.add(CANCER_BLOCK_ITEM)
+		}
+		.build()
+
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
@@ -29,5 +39,7 @@ object PreventTheSpread : ModInitializer {
 
 		Registry.register(Registries.BLOCK, Identifier(MOD_ID, "cancer_block"), CANCER_BLOCK)
 		Registry.register(Registries.ITEM, Identifier(MOD_ID, "cancer_block"), CANCER_BLOCK_ITEM)
+
+		Registry.register(Registries.ITEM_GROUP, Identifier(MOD_ID, "default"), ITEM_GROUP)
 	}
 }
