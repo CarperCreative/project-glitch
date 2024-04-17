c648c093bfda2c9270bcf3fe67f5412923b1260f
opl <4833621+opl@users.noreply.github.com>
2024-04-17 03:50:31 +0200

    Add basic cancer block with item

--- src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
@@ -1,6 +1,16 @@
 package com.carpercreative.preventthespread
 
+import com.carpercreative.preventthespread.block.CancerBlock
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.MapColor
+import net.minecraft.block.piston.PistonBehavior
+import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 object PreventTheSpread : ModInitializer {
@@ -8,10 +18,16 @@ object PreventTheSpread : ModInitializer {
 
 	private val logger = LoggerFactory.getLogger(MOD_ID)
 
+	val CANCER_BLOCK = CancerBlock(FabricBlockSettings.create().strength(4.0f).ticksRandomly().mapColor(MapColor.DARK_CRIMSON).pistonBehavior(PistonBehavior.BLOCK))
+	val CANCER_BLOCK_ITEM = BlockItem(CANCER_BLOCK, FabricItemSettings())
+
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
+
+		Registry.register(Registries.BLOCK, Identifier(MOD_ID, "cancer_block"), CANCER_BLOCK)
+		Registry.register(Registries.ITEM, Identifier(MOD_ID, "cancer_block"), CANCER_BLOCK_ITEM)
 	}
 }
---
+++ src/main/kotlin/com/carpercreative/preventthespread/block/CancerBlock.kt
@@ -0,0 +1,9 @@
+package com.carpercreative.preventthespread.block
+
+import net.minecraft.block.Block
+
+class CancerBlock(
+	settings: Settings,
+) : Block(settings) {
+
+}
