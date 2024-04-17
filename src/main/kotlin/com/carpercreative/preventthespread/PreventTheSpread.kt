3a3efa9401410b8077beed37db9782af424508c3
opl <4833621+opl@users.noreply.github.com>
2024-04-17 03:46:53 +0200

    Add and use MOD_ID constant

--- src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/PreventTheSpread.kt
@@ -4,7 +4,9 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object PreventTheSpread : ModInitializer {
-	private val logger = LoggerFactory.getLogger("preventthespread")
+	val MOD_ID = "preventthespread"
+
+	private val logger = LoggerFactory.getLogger(MOD_ID)
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
