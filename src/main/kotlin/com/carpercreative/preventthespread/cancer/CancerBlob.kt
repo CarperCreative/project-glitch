e3e9a185ba15e7a810cf5b49783554ede0bd4d43
opl <4833621+opl@users.noreply.github.com>
2024-04-18 08:21:26 +0200

    Add CancerType and CancerBlob

---
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerBlob.kt
@@ -0,0 +1,8 @@
+package com.carpercreative.preventthespread.cancer
+
+typealias BlobIdentifier = Int
+
+class CancerBlob(
+	val id: BlobIdentifier,
+	val type: CancerType,
+)
---
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerType.kt
@@ -0,0 +1,7 @@
+package com.carpercreative.preventthespread.cancer
+
+enum class CancerType {
+	PRE_CANCEROUS,
+	EARLY,
+	ADVANCED,
+}
