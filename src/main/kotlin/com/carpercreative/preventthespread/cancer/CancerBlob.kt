df56475aca429b30b3cd16db752fa07d463fa7ce
opl <4833621+opl@users.noreply.github.com>
2024-04-18 08:21:54 +0200

    Add CancerBlob persistence

--- src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerBlob.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerBlob.kt
@@ -1,8 +1,27 @@
 package com.carpercreative.preventthespread.cancer
 
+import net.minecraft.nbt.NbtCompound
+
 typealias BlobIdentifier = Int
 
 class CancerBlob(
 	val id: BlobIdentifier,
 	val type: CancerType,
-)
+) {
+	fun toNbt() = NbtCompound().apply {
+		putInt(KEY_VERSION, 1)
+		putInt(KEY_ID, id)
+		putString(KEY_TYPE, type.toString())
+	}
+
+	companion object {
+		private const val KEY_VERSION = "version"
+		private const val KEY_ID = "id"
+		private const val KEY_TYPE = "type"
+
+		fun NbtCompound.toCancerBlob() = CancerBlob(
+			getInt(KEY_ID),
+			CancerType.valueOf(getString(KEY_TYPE)),
+		)
+	}
+}
---
+++ src/main/kotlin/com/carpercreative/preventthespread/persistence/CancerBlobPersistentState.kt
@@ -0,0 +1,95 @@
+package com.carpercreative.preventthespread.persistence
+
+import com.carpercreative.preventthespread.PreventTheSpread
+import com.carpercreative.preventthespread.cancer.BlobIdentifier
+import com.carpercreative.preventthespread.cancer.CancerBlob
+import com.carpercreative.preventthespread.cancer.CancerBlob.Companion.toCancerBlob
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtList
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.world.PersistentState
+import net.minecraft.world.World
+
+class CancerBlobPersistentState : PersistentState() {
+	private var nextId = 0
+
+	private val cancerBlobs = hashMapOf<BlobIdentifier, CancerBlob>()
+
+	fun getCancerBlobByIdOrNull(id: BlobIdentifier): CancerBlob? {
+		return cancerBlobs[id]
+	}
+
+	fun getCancerBlobById(id: BlobIdentifier): CancerBlob {
+		return getCancerBlobByIdOrNull(id)
+			?: throw IllegalArgumentException("Requested cancer blob with id $id, which does not exist.")
+	}
+
+	fun createCancerBlob(callback: (id: BlobIdentifier) -> CancerBlob?): CancerBlob? {
+		val id = getNextId()
+		val cancerBlob = callback(id)
+		cancerBlob?.also {
+			cancerBlobs[id] = cancerBlob
+			markDirty()
+		}
+		return cancerBlob
+	}
+
+	fun removeCancerBlob(id: BlobIdentifier): CancerBlob? {
+		val cancerBlob = cancerBlobs.remove(id)
+		if (cancerBlob != null) {
+			markDirty()
+		}
+		return cancerBlob
+	}
+
+	private fun getNextId(): BlobIdentifier {
+		val id = nextId++
+		markDirty()
+		return id
+	}
+
+	override fun writeNbt(nbt: NbtCompound): NbtCompound {
+		nbt.putInt(KEY_VERSION, 1)
+
+		nbt.putInt(KEY_NEXT_ID, nextId)
+
+		val cancerBlobsNbt = NbtList()
+		for (cancerBlob in cancerBlobs.values) {
+			cancerBlobsNbt.add(cancerBlob.toNbt())
+		}
+		nbt.put(KEY_CANCER_BLOBS, cancerBlobsNbt)
+
+		return nbt
+	}
+
+	companion object {
+		private const val KEY_VERSION = "version"
+		private const val KEY_NEXT_ID = "nextId"
+		private const val KEY_CANCER_BLOBS = "cancerBlobs"
+
+		private val type = Type(
+			{ CancerBlobPersistentState() },
+			CancerBlobPersistentState::createFromNbt,
+			null,
+		)
+
+		fun createFromNbt(nbt: NbtCompound): CancerBlobPersistentState {
+			val state = CancerBlobPersistentState()
+
+			state.nextId = nbt.getInt(KEY_NEXT_ID)
+
+			val cancerBlobsNbt = nbt.getList(KEY_CANCER_BLOBS, NbtElement.COMPOUND_TYPE.toInt())
+			for (cancerBlobNbt in cancerBlobsNbt) {
+				val cancerBlob = (cancerBlobNbt as NbtCompound).toCancerBlob()
+				state.cancerBlobs[cancerBlob.id] = cancerBlob
+			}
+
+			return state
+		}
+
+		fun ServerWorld.getCancerBlobPersistentState(): CancerBlobPersistentState {
+			return server.getWorld(World.OVERWORLD)!!.persistentStateManager.getOrCreate(type, PreventTheSpread.MOD_ID)
+		}
+	}
+}
