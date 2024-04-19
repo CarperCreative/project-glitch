7fd5f378ce6d525b1884e9436ec8edf256ab97d5
opl <4833621+opl@users.noreply.github.com>
2024-04-20 01:08:31 +0200

    Add heat and overheated model predicate providers for radiation staff

--- src/client/kotlin/com/carpercreative/preventthespread/PreventTheSpreadClient.kt
+++ src/client/kotlin/com/carpercreative/preventthespread/PreventTheSpreadClient.kt
@@ -2,6 +2,7 @@ package com.carpercreative.preventthespread
 
 import com.carpercreative.preventthespread.item.DebugToolItem
 import com.carpercreative.preventthespread.item.ProbeItem
+import com.carpercreative.preventthespread.item.RadiationStaffItem
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.item.ModelPredicateProviderRegistry
 import net.minecraft.util.Identifier
@@ -14,6 +15,13 @@ object PreventTheSpreadClient : ClientModInitializer {
 
 		ModelPredicateProviderRegistry.register(PreventTheSpread.PROBE_ITEM, Identifier("sample")) { stack, clientWorld, livingEntity, seed ->
 			if (ProbeItem.containsSample(stack)) 1f else 0f
+		}
+
+		ModelPredicateProviderRegistry.register(PreventTheSpread.RADIATION_STAFF_ITEM, Identifier("heat")) { stack, clientWorld, livingEntity, seed ->
+			stack.damage.toFloat() / stack.maxDamage
+		}
+		ModelPredicateProviderRegistry.register(PreventTheSpread.RADIATION_STAFF_ITEM, Identifier("overheated")) { stack, clientWorld, livingEntity, seed ->
+			if (RadiationStaffItem.isOverheated(stack)) 1f else 0f
 		}
 	}
 }
