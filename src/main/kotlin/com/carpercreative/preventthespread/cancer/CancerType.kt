8ce659bb6760babce909f493cbf064b2cdd8d50f
opl <4833621+opl@users.noreply.github.com>
2024-04-19 22:45:27 +0200

    Add treatment types suitable for different cancer types

--- src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerType.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerType.kt
@@ -1,7 +1,20 @@
 package com.carpercreative.preventthespread.cancer
 
-enum class CancerType {
-	PRE_CANCEROUS,
-	EARLY,
-	ADVANCED,
+enum class CancerType(
+	val treatments: Array<TreatmentType>,
+) {
+	PRE_CANCEROUS(
+		arrayOf(TreatmentType.SURGERY, TreatmentType.TARGETED_DRUG),
+	),
+	EARLY(
+		arrayOf(TreatmentType.CHEMOTHERAPY, TreatmentType.RADIATION_THERAPY),
+	),
+	ADVANCED(
+		arrayOf(TreatmentType.SURGERY, TreatmentType.CHEMOTHERAPY),
+	),
+	;
+
+	fun isTreatmentValid(treatment: TreatmentType): Boolean {
+		return treatments.contains(treatment)
+	}
 }
---
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/TreatmentType.kt
@@ -0,0 +1,8 @@
+package com.carpercreative.preventthespread.cancer
+
+enum class TreatmentType {
+	SURGERY,
+	CHEMOTHERAPY,
+	RADIATION_THERAPY,
+	TARGETED_DRUG,
+}
