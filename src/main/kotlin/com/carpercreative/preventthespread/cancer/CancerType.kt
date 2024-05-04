766d9dcd52cc2f686700ec329aac27470512f9aa
opl <4833621+opl@users.noreply.github.com>
2024-05-04 14:48:26 +0200

    Add CancerType.displayName

--- src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerType.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/cancer/CancerType.kt
@@ -1,15 +1,22 @@
 package com.carpercreative.preventthespread.cancer
 
+import com.carpercreative.preventthespread.PreventTheSpread
+import net.minecraft.text.Text
+
 enum class CancerType(
+	val displayName: Text,
 	val treatments: Array<TreatmentType>,
 ) {
 	PRE_CANCEROUS(
+		Text.translatable("${PreventTheSpread.MOD_ID}.cancer_type.pre_cancerous"),
 		arrayOf(TreatmentType.SURGERY, TreatmentType.TARGETED_DRUG),
 	),
 	EARLY(
+		Text.translatable("${PreventTheSpread.MOD_ID}.cancer_type.early"),
 		arrayOf(TreatmentType.CHEMOTHERAPY, TreatmentType.RADIATION_THERAPY),
 	),
 	ADVANCED(
+		Text.translatable("${PreventTheSpread.MOD_ID}.cancer_type.advanced"),
 		arrayOf(TreatmentType.SURGERY, TreatmentType.CHEMOTHERAPY),
 	),
 	;
