89bcdde6053043675558e48d2646c5b79af57a48
opl <4833621+opl@users.noreply.github.com>
2024-05-07 23:59:36 +0200

    Lock all treatments other than surgery behind a recipe unlock

--- src/main/kotlin/com/carpercreative/preventthespread/datagen/ItemTagGenerator.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/datagen/ItemTagGenerator.kt
@@ -11,6 +11,11 @@ class ItemTagGenerator(
 	completableFuture: CompletableFuture<WrapperLookup>,
 ) : ItemTagProvider(output, completableFuture) {
 	override fun configure(arg: WrapperLookup) {
+		getOrCreateTagBuilder(PreventTheSpread.REQUIRES_RECIPE_TO_CRAFT_ITEM_TAG)
+			.add(PreventTheSpread.CHEMOTHERAPEUTIC_DRUG_ID)
+			.add(PreventTheSpread.RADIATION_STAFF_ITEM_ID)
+			.add(PreventTheSpread.TARGETED_DRUG_INJECTOR_ID)
+
 		getOrCreateTagBuilder(PreventTheSpread.SURGERY_TOOL_ITEM_TAG)
 			.add(PreventTheSpread.SURGERY_AXE_ITEM)
 			.add(PreventTheSpread.SURGERY_HOE_ITEM)
