981cb6f21f3d47f40da22b27d41ba36713c1ac97
opl <4833621+opl@users.noreply.github.com>
2024-05-09 21:28:06 +0200

    Add PlayerEntity.hasAdvancement helper

---
+++ src/main/kotlin/com/carpercreative/preventthespread/util/PlayerEntityExt.kt
@@ -0,0 +1,25 @@
+package com.carpercreative.preventthespread.util
+
+import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Identifier
+
+fun PlayerEntity.hasAdvancement(identifier: Identifier): Boolean {
+	when (this) {
+		is ServerPlayerEntity -> {
+			val advancement = server.advancementLoader.get(identifier)
+				?: return false
+			return advancementTracker.getProgress(advancement).isDone
+		}
+		is ClientPlayerEntity -> {
+			val advancementHandler = networkHandler.advancementHandler
+			val advancement = advancementHandler.get(identifier)
+				?: return false
+			val advancementProgress = advancementHandler.advancementProgresses_accessor[advancement]
+				?: return false
+			return advancementProgress.isDone
+		}
+		else -> throw IllegalArgumentException("Received unknown player type: $this")
+	}
+}
