29650d29a9a8e3edf1448e8c8fc197a2057e0401
opl <4833621+opl@users.noreply.github.com>
2024-07-06 18:46:33 +0200

    Play sound when spread percentage goes up

--- src/main/kotlin/com/carpercreative/preventthespread/networking/GlitchProgressPacket.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/networking/GlitchProgressPacket.kt
@@ -1,10 +1,14 @@
 package com.carpercreative.preventthespread.networking
 
+import com.carpercreative.preventthespread.PreventTheSpread
 import com.carpercreative.preventthespread.client.ClientStorage
+import kotlin.math.roundToInt
 import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.network.ClientPlayNetworkHandler
 import net.minecraft.network.PacketByteBuf
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvent
 
 class GlitchProgressPacket(
 	progress: Float,
@@ -20,10 +24,17 @@ class GlitchProgressPacket(
 	}
 
 	companion object {
+		private val GLITCH_SPREADS_SOUND_EVENT = SoundEvent.of(PreventTheSpread.identifier("glitch_spreads"))
+
 		fun handle(client: MinecraftClient, handler: ClientPlayNetworkHandler, buf: PacketByteBuf, responseSender: PacketSender) {
+			val previousProgress = ClientStorage.glitchProgress
 			val packet = GlitchProgressPacket(buf)
 
 			ClientStorage.glitchProgress = packet.progress
+
+			if (previousProgress > 0f && (packet.progress * 100f).roundToInt() > (previousProgress * 100f).roundToInt()) {
+				client.player?.playSound(GLITCH_SPREADS_SOUND_EVENT, SoundCategory.AMBIENT, 1f, 0.3f)
+			}
 		}
 	}
 }
