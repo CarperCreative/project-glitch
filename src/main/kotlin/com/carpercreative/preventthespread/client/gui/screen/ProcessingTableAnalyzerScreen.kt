b33d7117ff3651bddbbbff0c08f15f68f151c175
opl <4833621+opl@users.noreply.github.com>
2024-07-30 16:24:32 +0200

    Show preview of the front page of a book inserted into analyzer as item tooltip

--- src/main/kotlin/com/carpercreative/preventthespread/client/gui/screen/ProcessingTableAnalyzerScreen.kt
+++ src/main/kotlin/com/carpercreative/preventthespread/client/gui/screen/ProcessingTableAnalyzerScreen.kt
@@ -4,17 +4,27 @@ import com.carpercreative.preventthespread.PreventTheSpread
 import com.carpercreative.preventthespread.blockEntity.ProcessingTableAnalyzerBlockEntity
 import com.carpercreative.preventthespread.screen.ProcessingTableAnalyzerScreenHandler
 import com.carpercreative.preventthespread.screen.slot.AnalyzerBookSlot
+import com.google.common.collect.Lists
 import kotlin.math.roundToInt
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.ingame.BookScreen.WrittenBookContents
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.item.WrittenBookItem
+import net.minecraft.text.OrderedText
 import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 
 class ProcessingTableAnalyzerScreen(
 	handler: ProcessingTableAnalyzerScreenHandler,
 	inventory: PlayerInventory,
 	title: Text,
 ) : HandledScreen<ProcessingTableAnalyzerScreenHandler>(handler, inventory, title) {
+	private var cachedBookStack: ItemStack? = null
+	private var cachedBookTooltip = Lists.newArrayList<OrderedText>()
+
 	override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
 		super.render(context, mouseX, mouseY, delta)
 		drawMouseoverTooltip(context, mouseX, mouseY)
@@ -37,17 +47,56 @@ class ProcessingTableAnalyzerScreen(
 	}
 
 	override fun drawMouseoverTooltip(context: DrawContext, x: Int, y: Int) {
-		if (handler.cursorStack.isEmpty && (focusedSlot as? AnalyzerBookSlot)?.hasStack() == false) {
-			context.drawTooltip(textRenderer, hintTooltip, x, y)
-		} else {
-			super.drawMouseoverTooltip(context, x, y)
+		val analyzerBookSlot = focusedSlot as? AnalyzerBookSlot
+
+		if (handler.cursorStack.isEmpty && analyzerBookSlot != null) {
+			if (!analyzerBookSlot.hasStack()) {
+				// Show hint about the purpose of the book slot.
+				context.drawTooltip(textRenderer, hintTooltip, x, y)
+				return
+			}
+
+			// Show a preview of the book contents.
+			val bookStack = analyzerBookSlot.stack
+			if (
+				bookStack != null
+				&& bookStack.isOf(Items.WRITTEN_BOOK)
+				&& WrittenBookItem.getPageCount(bookStack) >= 1
+			) {
+				if (cachedBookStack != bookStack) {
+					cachedBookStack = bookStack
+
+					// Create a tooltip and cache it.
+					cachedBookTooltip.clear()
+					cachedBookTooltip.add(BOOK_PREVIEW_TITLE_TEXT.asOrderedText())
+
+					val pageText = WrittenBookContents(bookStack).getPage(0)
+						// Copied from BookScreen.
+						?.let { textRenderer.wrapLines(it, 114) }
+						// Remove empty lines from the end of the tooltip.
+						?.dropLastWhile { text -> textRenderer.getWidth(text) <= 0 }
+
+					pageText?.also { cachedBookTooltip.addAll(it) }
+				}
+
+				context.drawOrderedTooltip(textRenderer, cachedBookTooltip, x, y)
+				return
+			} else {
+				// Forget the item as and tooltip as soon as possible to prevent hanging onto stale references.
+				cachedBookStack = null
+				cachedBookTooltip.clear()
+			}
 		}
+
+		super.drawMouseoverTooltip(context, x, y)
 	}
 
 	companion object {
 		private val TEXTURE = PreventTheSpread.identifier("textures/gui/container/processing_table_analyzer.png")
 		private val EMPTY_PROGRESS_TEXTURE = PreventTheSpread.identifier("container/processing_table_analyzer/progress_empty")
 		private val FULL_PROGRESS_TEXTURE = PreventTheSpread.identifier("container/processing_table_analyzer/progress_full")
+
+		private val BOOK_PREVIEW_TITLE_TEXT = Text.translatable("container.${PreventTheSpread.MOD_ID}.processing_table_analyzer.book_preview.title").formatted(Formatting.YELLOW)
 
 		private val hintTooltip = listOf(
 			Text.translatable("container.${PreventTheSpread.MOD_ID}.processing_table_analyzer.empty_book_tooltip.0"),
